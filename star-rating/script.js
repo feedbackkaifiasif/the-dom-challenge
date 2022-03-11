141466968ba7f5b3a8382403b0c641f1d1790092
masterpranay1 <rajpranaychottu@gmail.com>
2022-03-11 23:52:36 +0530

    star-rating completed

--- star-rating/script.js
+++ star-rating/script.js
@@ -1,9 +1,40 @@
-/*
- * Creates star rating functionality
- * @param el DOM Element
- * @param count Number of stars
- * @param callback Returns selected star count to callback
- */
-function Star(el, count, callback) {
-    
+const stars = document.querySelector('#stars__wrapper')
+const starsList = stars.querySelectorAll('.fa-star')
+const starCountDisplay = document.querySelector('#star-count-display')
+let STARCOUNT = 0
+
+const updateStarCountDisplay = () => {
+  starCountDisplay.textContent = STARCOUNT
 }
+
+const paintStar = (cnt = STARCOUNT) => {
+  starsList.forEach((star, i) => {
+    if(i < cnt) {
+      star.classList.remove('fa-regular')
+      star.classList.add('fa-solid')
+    } else {
+      star.classList.add('fa-regular')
+      star.classList.remove('fa-solid')
+    }
+  })
+  updateStarCountDisplay()
+}
+
+const handleStarHover = (e) => {
+  [ , cnt] = e.target.id.split('-') // star-3
+  paintStar(cnt) // creating a temorary count
+}
+const handleStarHoverEnd = (e) => {
+  paintStar() // paint back to global count
+}
+const handleStarClick = (e) => {
+  [ , cnt] = e.target.id.split('-') // star-3
+  STARCOUNT = cnt
+  paintStar()
+}
+
+starsList.forEach(star => {
+  star.addEventListener('mouseenter', handleStarHover)
+  star.addEventListener('mouseout', handleStarHoverEnd)
+  star.addEventListener('click', handleStarClick)
+})
