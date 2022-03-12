b3a0253b1912591613de3011ebbb37954f833836
masterpranay1 <rajpranaychottu@gmail.com>
2022-03-12 17:40:50 +0530

    pixel art challenge completed

---
+++ pixel-art/script.js
@@ -0,0 +1,68 @@
+const grid = document.querySelector('.grid')
+let CURRENTCOLOR = null
+let isMouseClicked = false
+
+function GridItem(className, bgcolor) {
+  const gridItem = document.createElement('span')
+  gridItem.className = `gridItem ${className}`
+  gridItem.style.background = bgcolor
+  return gridItem
+}
+
+const getRandomColor = () => {
+  const hue = Math.floor(Math.random() * 360)
+  return `hsl(${hue}, 100%, 75%)`
+}
+
+const addPaintingArea = () => {
+  for(let i = 0; i < 100; i++) {
+    grid.appendChild(new GridItem('painting-block', '#fbfffd'))
+  }
+}
+const addLastRow = () => {
+  // this row contains colors
+  for(let i = 0; i < 10; i++) {
+   grid.appendChild(new GridItem('paint-block last-row', getRandomColor()))
+  }
+}
+
+const addEventToPaintBlocks = () => {
+  const paintBlockList = document.querySelectorAll('.paint-block')
+  paintBlockList.forEach(block => {
+    block.addEventListener('click', e => {
+      CURRENTCOLOR = e.target.style.backgroundColor
+    })
+  })
+}
+
+const handleMouseDown = (e) => {
+  console.log('mouse down');
+  if(CURRENTCOLOR) e.target.style.background = CURRENTCOLOR
+  isMouseClicked = true
+}
+const handleMouseMove = e => {
+  console.log('mouse move');
+  if(isMouseClicked && CURRENTCOLOR) e.target.style.background = CURRENTCOLOR
+}
+const handleMouseUp = e => {
+  console.log('mouse up');
+  isMouseClicked = false
+}
+
+const addEventToPaintingBlocks = () => {
+  const paintingBlockList = document.querySelectorAll('.painting-block')
+  paintingBlockList.forEach(block => {
+    block.addEventListener('mousedown', handleMouseDown)
+    block.addEventListener('mousemove', handleMouseMove)
+    block.addEventListener('mouseup', handleMouseUp)
+    block.addEventListener('dbclick', handleMouseUp)
+  })
+}
+
+const paintGridWithItems = () => {
+  addPaintingArea()
+  addLastRow()
+  addEventToPaintBlocks()
+  addEventToPaintingBlocks()
+}
+paintGridWithItems()
